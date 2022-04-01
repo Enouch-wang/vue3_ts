223f92780fe8c85b72845bd732a4277723ef2f72
早晨的阳光 <43134635+Enouch-wang@users.noreply.github.com>
2022-04-01 15:50:21 +0800

    Add files via upload

---
+++ src/views/home/home.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="home-box">
+        <div class="title">简洁/实用<span>vue3.0+ts</span> 现成模板</div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    setup() {
+        
+    },
+})
+</script>
+<style lang="scss" scoped >
+    .home-box{
+        text-align: center;
+       .title{
+           font-size: 40px;
+           font-weight: 600;
+           margin: 50px 0 30px;
+           span{
+               color: red;
+           }
+       }
+       
+    }
+    @keyframes icon-svg{
+        0%{
+            opacity: 0.5;
+        }
+        50%{
+           opacity: 1;
+        }
+        100%{
+           opacity: 0.5;
+        }
+    }
+</style>
